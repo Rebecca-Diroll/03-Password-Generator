f89c5a921af62b888a97933c36bd9d41e4d51241
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-02-13 16:54:00 -0500

    Add function to write the generated password to the webpage.

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,14 @@ let useNumber;
 let useSymbol;
 
 document.querySelector("#generate").addEventListener("click", writePassword)
-/*
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-*/
+
 function generatePassword() {
   userInput();
   var output = characterOptions(passwordLength);
